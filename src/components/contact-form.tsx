b3d67ac129e17cd4c95d1375b759323f61e14174
xhans <hanwinhtun334@gmail.com>
2024-02-09 13:56:58 -0800

    Update contact-form.tsx

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -64,7 +64,7 @@ export function ContactForm() {
 						name="email"
 						id="email"
 						required
-						placeholder="hi@hantun.sh"
+						placeholder="hi@hantun.ca"
 						className="w-full"
 					/>
 				</label>

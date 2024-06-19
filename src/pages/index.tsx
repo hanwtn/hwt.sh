8a92592a0ee3507f15c5ebb8657028de4a72221c
Han <hanwinhtun334@gmail.com>
2024-06-19 15:05:49 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -513,6 +513,7 @@ export default function Home(props: Props) {
 			<footer className="mx-auto flex max-w-3xl items-center justify-center px-6 pb-12 text-sm text-neutral-600 [&_a:hover]:text-neutral-400 [&_a]:p-4 [&_a]:transition-colors">
 				{/* <Link href="https://github.com/alii/website">Source</Link> */}
 				<Link href="/stats">Stats</Link>
+				<Link href="https://hanwintun.com">Archived Photos</Link>
 				{/* <Link href="/experiments">Experiments</Link> */}
 			</footer>
 		</div>

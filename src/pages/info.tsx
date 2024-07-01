8ec31ea7a377f17d30029509fbe130d33462cb82
han <hanwinhtun334@gmail.com>
2024-07-01 10:33:47 -0700

    Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -226,7 +226,7 @@ export default function Home(props: Props) {
 						<span className="space-y-0.5 px-6 pb-6">
 							<span className="block font-semibold">Photos</span>
 
-							<span className="block text-sm">moments in my life</span>
+							<span className="block text-sm">random moments</span>
 						</span>
 					</Link>
 				</CardHoverEffect>
